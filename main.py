7345224816a8653fe0cec32494f57f12f474fd35
Coles <coles.81@asc.ohio-state.edu>
2017-12-11 10:57:43 -0500

    GUI construction

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+@title RSA_Metrology
+@author: Rebecca Coles
+Updated on Dec 11, 2017
+Created on Dec 11, 2017
+'''
+
+# Import #######################################################################################
+from tkinter import Tk
+from inputGUI import inputGUI
+################################################################################################
+
+if __name__ == '__main__':
+    root = Tk()
+    iGUI = inputGUI(root)
+    root.mainloop()
