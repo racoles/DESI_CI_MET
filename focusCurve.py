29c4fee79124ffb7eaa2efc7b48b02351b4bc781
Coles <coles.81@asc.ohio-state.edu>
2017-12-12 14:13:29 -0500

    Adding Focus Curve

--- focusCurve.py
+++ focusCurve.py
@@ -14,8 +14,11 @@ stdFocusCurve
 '''
 
 # Import #######################################################################################
-from numpy import std, zeros, polyfit
+from numpy import std, zeros, poly1d, polyfit, linspace, polyder
 from matplotlib.pyplot import ioff, xlabel, ylabel, title, grid, figure, text
+import os
+from os.path import basename
+from operator import itemgetter
 ################################################################################################
 
 class focusCurve(object):
@@ -96,3 +99,55 @@ class focusCurve(object):
         fig2.savefig('std_vs_position-fitted.png') 
         #return best focus
         return xInter
+    
+    def fileNameToInt(self, filelist):
+        '''
+        Create x values by remove extension from filenames and converting them to ints
+        '''
+        xx = []
+        [xx.append(os.path.splitext(filelist[image])[0]) for image in range(len(filelist))]
+        for ii in range(len(xx)):#get basename of files
+            xx[ii] = int(basename(xx[ii]))
+        return xx
+    
+    def zipAndSort(self, xx, yy):
+        '''
+        Zip xx and yy values into array of tulups
+        Sort list by distance (x) so xx (distances) are in the proper order in the plot
+        '''
+        #zip xx and yy
+        xy = []
+        [xy.append(jj) for jj in zip(xx, yy)]
+        
+        #sort list by distance (x)
+        sortedXY = sorted(xy, key=itemgetter(0))
+        
+        #seperate into X and Y
+        sortedX = [kk[0] for kk in sortedXY]
+        sortedY = [ll[1] for ll in sortedXY]
+        
+        return sortedX, sortedY
+    
+    def xyPolyFit(self, xx, yy, order):
+        '''
+        Calculate polynomial fit (order given)
+        Calculate new x's and y's for plotting
+        Find slope = 0 for fit, this will be used to split the data to a left and right liner fit
+        Find the first derivative of the poly1d
+        Solve deriv =ax + b for deriv = 0 (point of best focus)
+        '''
+        #calculate polynomial
+        funct = poly1d(polyfit(xx, yy, order))
+        
+        #calculate new x's and y's
+        xFit = linspace(xx[0], xx[-1])
+        yFit = funct(xFit)
+        
+        #find slope = 0 for fit, this will be used to split the data to a left and right liner fit
+        ##find the first derivative of the poly1d
+        deriv = polyder(funct)
+        
+        ##solve deriv =ax + b for deriv = 0 (used to split data into left and right sides of the curve)
+        bestFocus = (0-deriv.c[1])/(deriv.c[0])
+        
+        return xFit, yFit, funct, bestFocus
