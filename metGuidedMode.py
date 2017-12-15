74b2d1f6b61a3b68ee49fc374eeb305789ebbc72
Coles <coles.81@asc.ohio-state.edu>
2017-12-15 14:55:39 -0500

    Adding guided mode

--- metGuidedMode.py
+++ metGuidedMode.py
@@ -39,7 +39,8 @@ class metGuidedMode(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
 
-        pages = (startPage(consoleLog, logFile), fifPage(consoleLog, logFile), conclusion(consoleLog, logFile))
+        print(logFile)
+        pages = (startPage, fifPage, conclusion)
         #
         #"RefFIF", 
         #"NFIF", "EFIF", "SFIF", "WFIF", 
@@ -64,7 +65,7 @@ class metGuidedMode(tk.Tk):
         
 class startPage(tk.Frame):
 
-    def __init__(self, parent, controller, consoleLog, logFile):
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
         label = tk.Label(self, text="Start Page", font=frameFont)
         label.pack(pady=10,padx=10)
@@ -80,7 +81,7 @@ class startPage(tk.Frame):
 
 class fifPage(tk.Frame):
 
-    def __init__(self, parent, controller, consoleLog, logFile):
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Page One!!!", font=frameFont)
         label.pack(pady=10,padx=10)
@@ -96,7 +97,7 @@ class fifPage(tk.Frame):
 
 class conclusion(tk.Frame):
 
-    def __init__(self, parent, controller, consoleLog, logFile):
+    def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Page Two!!!", font=frameFont)
         label.pack(pady=10,padx=10)
