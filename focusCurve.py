1d03fea464b98b3fa60c06b84b036b66f212bc43
Coles <coles.81@asc.ohio-state.edu>
2017-12-18 10:25:27 -0500

    Adding guided mode

--- focusCurve.py
+++ focusCurve.py
@@ -25,8 +25,8 @@ xyPolyFit
 '''
 
 # Import #######################################################################################
-from numpy import std, zeros, poly1d, polyfit, linspace, polyder
-from matplotlib.pyplot import ioff, xlabel, ylabel, title, grid, figure, text
+import numpy as np
+import matplotlib.pyplot as py
 import os
 from os.path import basename
 from operator import itemgetter
@@ -45,15 +45,15 @@ class focusCurve(object):
         note: assumes filenames are distances (int)
         '''
         # Turn interactive plotting off
-        ioff()
+        py.ioff()
         
         #initialize std array
-        stdList = zeros(imageArray4D.shape[0])
+        stdList = np.zeros(imageArray4D.shape[0])
         
         #get stds
         for image in range(imageArray4D.shape[0]):
             flattenedArray = imageArray4D[image].flatten()
-            stdList[image] = std(flattenedArray)
+            stdList[image] = np.std(flattenedArray)
         
         #create x values by remove extension from filenames and converting them to ints
         xx = self.fileNameToInt(filelist)
@@ -76,8 +76,8 @@ class focusCurve(object):
         sortedYR = sortedY[len(sortedXL):]
         
         #linear fit each side
-        mL, bL = polyfit(sortedXL, sortedYL, 1) #left
-        mR, bR = polyfit(sortedXR, sortedYR, 1) #right
+        mL, bL = np.polyfit(sortedXL, sortedYL, 1) #left
+        mR, bR = np.polyfit(sortedXR, sortedYR, 1) #right
         
         #find intercept of the linear fits
         # want position where XL = XR and YL = YR using y =mx +b:
@@ -90,19 +90,19 @@ class focusCurve(object):
         sortedXR.append(xInter)
         
         ################## plot stds ##################
-        fig2 = figure()
+        fig2 = py.figure()
         ax2 = fig2.add_subplot(111)
         ax2.plot(sortedX, sortedY, 'ro', xFit, yFit, 
                  sortedXL, [mL*ll+bL for ll in sortedXL], 
                  sortedXR, [mR*mm+bR for mm in sortedXR])
-        xlabel('Focal Position (microns)')
-        ylabel('Standard Deviation')
-        title('Standard Deviation versus Distance')
-        text(0, 0, 'Left Linear Fit: y = ' + str(mL) + ' x + ' + str(bL) + 
+        py.xlabel('Focal Position (microns)')
+        py.ylabel('Standard Deviation')
+        py.title('Standard Deviation versus Distance')
+        py.text(0, 0, 'Left Linear Fit: y = ' + str(mL) + ' x + ' + str(bL) + 
              '\n\nRight Linear Fit: y = ' + str(mR) + ' x + ' + str(bR) + 
              '\n\nPolynomial Fit:\n        ' + str(f2) +
              '\n\nPolynomial Fit Max Distance= ' + str(xSplitPoint)[0:3] + ' um\n', fontsize = 7, transform=ax2.transAxes)
-        grid(True)
+        py.grid(True)
         ax2.annotate('Best Focus = ' + str(xInter)[0:5] + ' um', xy=(xInter, yInter), 
                      xytext=(xInter+1, yInter+1), fontsize = 7,)
          
@@ -148,15 +148,15 @@ class focusCurve(object):
         Solve deriv =ax + b for deriv = 0 (point of best focus)
         '''
         #calculate polynomial
-        funct = poly1d(polyfit(xx, yy, order))
+        funct = np.poly1d(np.polyfit(xx, yy, order))
         
         #calculate new x's and y's
-        xFit = linspace(xx[0], xx[-1])
+        xFit = np.linspace(xx[0], xx[-1])
         yFit = funct(xFit)
         
         #find slope = 0 for fit, this will be used to split the data to a left and right liner fit
         ##find the first derivative of the poly1d
-        deriv = polyder(funct)
+        deriv = np.polyder(funct)
         
         ##solve deriv =ax + b for deriv = 0 (used to split data into left and right sides of the curve)
         bestFocus = (0-deriv.c[1])/(deriv.c[0])
