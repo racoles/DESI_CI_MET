3dca772b06c1a8e7c5b7950d42021dc327bbf754
Coles <coles.81@asc.ohio-state.edu>
2018-01-18 16:55:00 -0500

    adding CCD tip/tilt/z

---
+++ tipTiltZCCD.py
@@ -0,0 +1,14 @@
+'''
+@title tipTiltZCCD
+@author: Rebecca Coles
+Updated on Jan 19, 2018
+Created on Jan 18, 2018
+
+tipTiltZCCD
+This module holds a series of functions that I use find the tip/tilt/Z of a CCD on the DESI CI.
+
+Modules:
+'''
+
+# Import #######################################################################################
+################################################################################################
