afb8536f3bc28001914730821e5d905dcbf68197
Coles <coles.81@asc.ohio-state.edu>
2018-01-18 12:03:11 -0500

    adding FIF guided mode buttons

---
+++ metManualMode.py
@@ -0,0 +1,12 @@
+'''
+@title metManualMode
+@author: Rebecca Coles
+Updated on Jan 18, 2018
+Created on Jan 18, 2018
+
+metManualMode
+Guided mode for DESI CI metrology.
+
+Classes and Modules:
+
+'''
