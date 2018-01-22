fe7ddb115b7d546c632b117b739029c9fbe6bdde
Coles <coles.81@asc.ohio-state.edu>
2018-01-22 16:15:47 -0500

    adding CCD tip/tilt/z

---
+++ CCDOpsPlanetMode.py
@@ -0,0 +1,16 @@
+'''
+@title CCDOpsPlanetMode
+@author: Rebecca Coles
+Updated on Jan 22, 2018
+Created on Jan 22, 2018
+
+CCDOpsPlanetMode
+This module holds a series of functions that I use find offsets when using the
+CCDOps software in "planet" imaging mode.
+
+Modules:
+'''
+
+# Import #######################################################################################
+import numpy as np
+################################################################################################
