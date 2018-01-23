437fe9ec15d6031dbf60e9b40cf8192603f6e2f2
Coles <coles.81@asc.ohio-state.edu>
2018-01-23 13:50:18 -0500

    adding CCDOps planet mode

--- CCDOpsPlanetMode.py
+++ CCDOpsPlanetMode.py
@@ -13,4 +13,23 @@ Modules:
 
 # Import #######################################################################################
 import numpy as np
+from astropy.io import fits
 ################################################################################################
+
+class CCDOpsPlanetMode(object):
+    
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    def readFitsHeader(self, imageArray4D, planetModeBool):
+        '''
+        Return offsets (in pixels) if an image is taken in CCDOps
+        software Planet Mode.
+        '''
+        ###########################################################################
+        ###planetModeBool == True Indicates Subframe
+        ###########################################################################
+        if planetModeBool == True:
+            #read header of first image
+            imageArray4D[0]
