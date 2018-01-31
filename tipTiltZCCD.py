54283b8c0a264d831dfaf0d7c436f329d61e96c7
racoles <shouraku@gmail.com>
2018-01-31 15:00:24 -0500

    setting up manual tip/tilt/z

--- tipTiltZCCD.py
+++ tipTiltZCCD.py
@@ -81,11 +81,31 @@ class tipTiltZCCD(object):
         ###Boundry Condition Check 
         ###########################################################################
         faah = fileAndArrayHandling()
-        faah.pageLogging(self.consoleLog, self.logFile, 
+        faah.pageLogging(consoleLog, logFile, 
                                       "Checking " + str(CCDLabel) + " TILT:")
         #N,W,S,E
+        if CCDLabel == "NCCD" or CCDLabel == "WCCD" or CCDLabel == "SCCD" or CCDLabel == "ECCD": 
+                faah.pageLogging(consoleLog, logFile, 
+                                        "    Condition #1: A(Z)_measured = A(Z)_nominal\n" + 
+                                        "        A(Z)_measured = " + str(Az) + "um\n" + 
+                                        "        A(Z)_nominal = " + str(Az_nominal) + "um\n" )
+                faah.pageLogging(consoleLog, logFile, 
+                                        "    Condition #2: B(Z)_measured = C(Z)_measured = B(Z)_nominal = C(Z)_nominal\n" + 
+                                        "        B(Z)_measured = " + str(Bz) + "um\n" + 
+                                        "        B(Z)_nominal = " + str(Bz_nominal) + "um\n" +
+                                        "        C(Z)_measured = " + str(Cz) + "um\n" +     
+                                        "        C(Z)_nominal = " + str(Cz_nominal) + "um\n") 
         #C
+        if CCDLabel == "CCCD":
+            faah.pageLogging(consoleLog, logFile, 
+                                        "    Condition #3: [A(Z) = B(Z) = C(Z)]_measured  = [A(Z) = B(Z) = C(Z)]_nominal\n" + 
+                                        "        A(Z)_measured = " + str(Az) + "um\n" + 
+                                        "        B(Z)_measured = " + str(Bz) + "um\n" + 
+                                        "        C(Z)_measured = " + str(Cz) + "um\n")
         #Other
+        if CCDLabel == "Other":
+            faah.pageLogging(consoleLog, logFile, 
+                                        "CCD selection: Other. Not able to calculate Tilt.")
 
     def ZCCD(self, Az, Bz, Cz, Az_nominal, Bz_nominal, Cz_nominal, CCDLabel, consoleLog, logFile):
         '''
