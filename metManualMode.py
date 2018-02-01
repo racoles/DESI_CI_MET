0941a372e7ae5f474b3326896e2617b40e2bf0a9
racoles <shouraku@gmail.com>
2018-02-01 10:37:31 -0500

    setting up manual tip/tilt/z

--- metManualMode.py
+++ metManualMode.py
@@ -198,18 +198,18 @@ class metManualMode(tk.Tk):
         xInterA = fC.stdFocusCurve(self.CCDSelection, imageArray4DA, filelistA, pointLabel = "A")
         nominalZA = fC.asphericFocalCurve(fC.trianglePonitCCDLocationsCS5[self.CCDSelection + 'A'][0], fC.trianglePonitCCDLocationsCS5[self.CCDSelection + 'A'][1])
         faah.pageLogging(self.consoleLog, self.logFile, 
-                                      str(self.CCDSelection) + "A(Best Focus):" + str(xInterA) + "um\n" + str(self.CCDSelection) + "A(Nominal Z):" + str(nominalZA) + "um")
+                                      str(self.CCDSelection) + " A (Best Focus):" + str(xInterA) + "um\n" + str(self.CCDSelection) + " A (Nominal Z):" + str(nominalZA) + "um")
 
         #B
         xInterB = fC.stdFocusCurve(self.CCDSelection, imageArray4DB, filelistB, pointLabel = "B")
         nominalZB = fC.asphericFocalCurve(fC.trianglePonitCCDLocationsCS5[self.CCDSelection + 'B'][0], fC.trianglePonitCCDLocationsCS5[self.CCDSelection + 'B'][1])
         faah.pageLogging(self.consoleLog, self.logFile, 
-                                      str(self.CCDSelection) + "B(Best Focus):" + str(xInterB) + "um\n" + str(self.CCDSelection) + "B(Nominal Z):" + str(nominalZB) + "um") 
+                                      str(self.CCDSelection) + " B (Best Focus):" + str(xInterB) + "um\n" + str(self.CCDSelection) + " B (Nominal Z):" + str(nominalZB) + "um") 
         #C
         xInterC = fC.stdFocusCurve(self.CCDSelection, imageArray4DC, filelistC, pointLabel = "C")
         nominalZC = fC.asphericFocalCurve(fC.trianglePonitCCDLocationsCS5[self.CCDSelection + 'C'][0], fC.trianglePonitCCDLocationsCS5[self.CCDSelection + 'C'][1])
         faah.pageLogging(self.consoleLog, self.logFile, 
-                                      str(self.CCDSelection) + "C(Best Focus):" + str(xInterC) + "um\n" + str(self.CCDSelection) + "C(Nominal Z):" + str(nominalZC) + "um")
+                                      str(self.CCDSelection) + " C (Best Focus):" + str(xInterC) + "um\n" + str(self.CCDSelection) + " C (Nominal Z):" + str(nominalZC) + "um")
         
         ###########################################################################
         ###Check Tip and Tilt
