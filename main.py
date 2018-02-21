854174e4ae72369ea329f8973d130dea3707f12d
Coles <coles.81@asc.ohio-state.edu>
2018-02-21 14:12:30 -0500

    comment edit

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 '''
 @title DESI_CI_MET
 @author: Rebecca Coles
-Updated on Dec 15, 2017
+Updated on Feb 21, 2017
 Created on Dec 11, 2017
 
 Using external packages:
     astropy
     matplotlib
     opencv-python
+    
+Testing:
+    DESI_CI_MET_1.0: DESI CI Camera Back Focus Focus Curve Modules Test (02/22/2018)
+Pending Testing:
+    Omatech Sheffield Cordax RS 70 DCC
 '''
 
 # Import #######################################################################################
