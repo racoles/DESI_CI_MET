51910ff5fb9ea13c2f293a7fa1f0c25ba0555f2a
Coles <coles.81@asc.ohio-state.edu>
2018-03-21 15:19:35 -0400

    adding Rz camera position check

--- cs5Offsets.py
+++ cs5Offsets.py
@@ -47,13 +47,13 @@ class cs5Offsets(object):
         top.title("DESI CI Metrology Software Calibration")
         
         #Manual Mode Description
-        tk.Label(top, text="Goal", justify="left", font = '-weight bold').grid(row=1, column=0, sticky='W')
+        tk.Label(top, text="Goal", justify="left", font = '-weight bold').grid(row=1, column=0, sticky='W', columnspan=2)
         tk.Label(top, text="To find the CS5 location of an illuminated object that is imaged using the SBIG ST-I and STXL-6303 cameras" + 
-                 "that is attached to the DMM, while the DMM is attached to a CMM.\n", wraplength=700, justify="left").grid(row=2, column=0, sticky='W')
-        Separator(top, orient="horizontal").grid(row=3, column=0, sticky='ew')
+                 "that is attached to the DMM, while the DMM is attached to a CMM.\n", wraplength=700, justify="left").grid(row=2, column=0, sticky='W', columnspan=2)
+        Separator(top, orient="horizontal").grid(row=3, column=0, sticky='ew', columnspan=2)
         
         #Offset 1: Light from 100um DMM Pinhole in ST-I Image (Needed for CCD Metrology Measurements)
-        tk.Label(top, text="Light from 100um DMM Pinhole in ST-I Image (Needed for CCD Metrology Measurements)", font = '-weight bold', justify="left").grid(row=4, column=0, sticky='W')
+        tk.Label(top, text="Light from 100um DMM Pinhole in ST-I Image (Needed for CCD Metrology Measurements)", font = '-weight bold', justify="left").grid(row=4, column=0, sticky='W', columnspan=2)
         tk.Label(top, text="When we project light from the DMM’s 100um pinhole onto a surface (usually a CCD), and image it" + 
                  " using the DMM’s ST-I camera, we need to know the distance from where the projected pinhole shows up on a ST-I" + 
                  " full frame image (in units of rows and columns) to the corner of that image (rows = 0, column = 0). The image" + 
@@ -62,7 +62,7 @@ class cs5Offsets(object):
                  " CMM, and the resulting distance is a static variable in the DESI CI Metrology Software.\n\n" +
                  "The current predetermined offsets show that the center the 100um pinhole on the DMM, when imaged with the ST-i" + 
                  " is located at:\n\n (row = " + str(self.PIDTSO_rows) + ", column = " + str(self.PIDTSO_columns) + ")\n", 
-                 wraplength=700, justify="left").grid(row=5, column=0, sticky='W')
+                 wraplength=700, justify="left").grid(row=5, column=0, sticky='W', columnspan=2)
                  
         faah = fileAndArrayHandling()
         faah.pageLogging(consoleLog, logFile, "Beginning Calibration Routine", calibration = True)
@@ -70,8 +70,8 @@ class cs5Offsets(object):
                  " is: (row = " + str(self.PIDTSO_rows) + ", column = " + str(self.PIDTSO_columns) + ")", calibration = True)
                        
         #Offset 2: CS5 Point on CI Dowel as shown in ST-I Image (Needed for FIF and CCD Metrology Measurements)
-        Separator(top, orient="horizontal").grid(row=6, column=0, sticky='ew')
-        tk.Label(top, text="CS5 Point on CI Dowel as shown in ST-I Image (Needed for FIF and CCD Metrology Measurements)", font = '-weight bold', justify="left").grid(row=7, column=0, sticky='W') 
+        Separator(top, orient="horizontal").grid(row=6, column=0, sticky='ew', columnspan=2)
+        tk.Label(top, text="CS5 Point on CI Dowel as shown in ST-I Image (Needed for FIF and CCD Metrology Measurements)", font = '-weight bold', justify="left").grid(row=7, column=0, sticky='W', columnspan=2) 
         tk.Label(top, text="When we tell the CS5 calibrated CMM to move to a given CS5 location, we will need to know where the" +
                  " specified point will show up in the associated ST-I image taken with the DMM for that point. To find this offset" + 
                  " in units of (rows, columns), perform the following  measurements prior to mounting the DMM on the CMM:\n\n" +
@@ -81,11 +81,11 @@ class cs5Offsets(object):
                  "\t2. After attaching the DMM to the CMM, tell the CMM to move to the CS5 location of the illuminated divot.\n\n" +
                  "\t3. Take an image of the illuminated divot using the ST-I camera on the DMM.\n\n" +
                  "\t4. Input the image into the DESI CI Metrology software using the input button below. The software will\n" +
-                 "\t\x20\x20\x20\x20locate the location of the illuminated divot in units of (rows, columns).\n", wraplength=700, justify="left").grid(row=8, column=0, sticky='W')
+                 "\t\x20\x20\x20\x20locate the location of the illuminated divot in units of (rows, columns).\n", wraplength=700, justify="left").grid(row=8, column=0, sticky='W', columnspan=2)
         offset2aButton = tk.Button(top, text="Enter CS5 coordinates of Illuminated Dowel Center", command=lambda: self._offset2a_moveToIlluminatedDowelAndProbe(offset2aButton, consoleLog, logFile))
-        offset2aButton.grid(row=9, column=0, sticky='W')        
+        offset2aButton.grid(row=9, column=0, sticky='W', columnspan=2)        
         offset2bButton = tk.Button(top, text="Load Image of Illuminated Divot", command=lambda: self._offset2b_moveToIlluminatedDowelAndImage(inputGUIcalibrationScreenButton, offset2bButton, consoleLog, logFile))
-        offset2bButton.grid(row=9, column=2, sticky='W')
+        offset2bButton.grid(row=9, column=1, sticky='W', columnspan=2)
         
     def _offset1_PinholeImageDistnceToSensorOrigin(self):
         '''
