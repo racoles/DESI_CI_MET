e09ced845bea650f0ba4de857758b6713f0a1d3e
Coles <coles.81@asc.ohio-state.edu>
2018-03-22 12:37:18 -0400

    adding Origin check

--- inputGUI.py
+++ inputGUI.py
@@ -35,8 +35,8 @@ class inputGUI(object):
     GUI for DESI CI metrology software
     '''
     
-    CCDSelection = " "
-    trianglePointLabel = " "
+    CCDSelection = ""
+    trianglePointLabel = ""
 
     def __init__(self, master):
         '''
@@ -249,106 +249,3 @@ class inputGUI(object):
                         "Iterative GMS Centroid (rows, columns): (" +  format(xCenFC, '.2f') + ' +/- ' + format(xErrFC, '.2f') + ', ' +
                          format(yCenFC, '.2f') + '+/-' + format(yErrFC, '.2f') + ')\n' +
                         "IDL DAOPHOT Centroid (rows, columns): (" + format(yCencF, '.2f') + ', ' + format(xCencF, '.2f')+ ')\n\n', doubleSpaceWithTime = False)
-        
-    def _checkCameraOriginLocation(self, consoleLog, logFile):
-        '''
-        Find the location of the CI camera's sensor origin in CS5 and instruct the user to view 
-        the origin with the DMM to ensure that the tip/tilt/focus pinhole triangle was placed properly
-        on the SBIt STXL sensor.
-        '''
-        ###########################################################################
-        ###Sensor Location menu
-        ###########################################################################
-        self._checkCameraOriginLocationSelectionWindow()
-        
-        ###########################################################################
-        ###Centroid Image
-        ########################################################################### 
-        
-        ###########################################################################
-        ###Calcuate the distance to the sensor origin using centroided image.
-        ###########################################################################        
-        cF = centroidFIF
-        CS5OriginX, CS5OriginY = cF.distanceFromPinholeImagetoOrigin(self, rows, columns, consoleLog, logFile, pixelSize, isCCD = False, CCDLabel = '', triangleLabel = '')
-    
-    def _checkCameraOriginLocationSelectionWindow(self):
-        '''
-        Find the location of the CI camera's sensor origin in CS5 and instruct the user to view 
-        the origin with the DMM to ensure that the tip/tilt/focus pinhole triangle was placed properly
-        on the SBIt STXL sensor.
-        '''
-        ###########################################################################
-        ###Construct menu
-        ###########################################################################   
-        top = tk.Toplevel()
-        top.title("Check Camera Origin")
-        
-        #CCD Location Description
-        tk.Label(top, text="Which CCD location would you like to measure?").grid(row=0, column=0, columnspan=2, sticky='W')
-        
-        # NCCD
-        Separator(top, orient="horizontal").grid(row=1, column=0, columnspan=4, sticky='ew')
-        tk.Label(top, text="NCCD").grid(row=2, column=0, columnspan=2, sticky='W')
-        NCCD_A = tk.Button(top, text="NCCD: A", command=lambda: self._setTrueAndExit(top, CCDLabel="NCCD", trianglePointLabel="A"))
-        NCCD_A.grid(row=3, column=0)
-        NCCD_B = tk.Button(top, text="NCCD: B", command=lambda: self._setTrueAndExit(top, CCDLabel="NCCD", trianglePointLabel="B"))
-        NCCD_B.grid(row=3, column=1)
-        NCCD_C = tk.Button(top, text="NCCD: C", command=lambda: self._setTrueAndExit(top, CCDLabel="NCCD", trianglePointLabel="C"))
-        NCCD_C.grid(row=3, column=2)
-        NCCD_Center = tk.Button(top, text="NCCD: Center", command=lambda: self._setTrueAndExit(top, CCDLabel="NCCD"))
-        NCCD_Center.grid(row=3, column=3)
-        
-        # WCCD
-        Separator(top, orient="horizontal").grid(row=4, column=0, columnspan=4, sticky='ew')
-        tk.Label(top, text="WCCD").grid(row=5, column=0, columnspan=2, sticky='W')
-        WCCD_A = tk.Button(top, text="WCCD: A", command=lambda: self._setTrueAndExit(top, CCDLabel="WCCD", trianglePointLabel="A"))
-        WCCD_A.grid(row=6, column=0)
-        WCCD_B = tk.Button(top, text="WCCD: B", command=lambda: self._setTrueAndExit(top, CCDLabel="WCCD", trianglePointLabel="B"))
-        WCCD_B.grid(row=6, column=1)
-        WCCD_C = tk.Button(top, text="WCCD: C", command=lambda: self._setTrueAndExit(top, CCDLabel="WCCD", trianglePointLabel="C"))
-        WCCD_C.grid(row=6, column=2)
-        WCCD_Center = tk.Button(top, text="WCCD: Center", command=lambda: self._setTrueAndExit(top, CCDLabel="WCCD"))
-        WCCD_Center.grid(row=6, column=3)
-        
-        # SCCD
-        Separator(top, orient="horizontal").grid(row=7, column=0, columnspan=4, sticky='ew')
-        tk.Label(top, text="SCCD").grid(row=8, column=0, columnspan=2, sticky='W')
-        SCCD_A = tk.Button(top, text="SCCD: A", command=lambda: self._setTrueAndExit(top, CCDLabel="SCCD", trianglePointLabel="A"))
-        SCCD_A.grid(row=9, column=0)
-        SCCD_B = tk.Button(top, text="SCCD: B", command=lambda: self._setTrueAndExit(top, CCDLabel="SCCD", trianglePointLabel="B"))
-        SCCD_B.grid(row=9, column=1)
-        SCCD_C = tk.Button(top, text="SCCD: C", command=lambda: self._setTrueAndExit(top, CCDLabel="SCCD", trianglePointLabel="C"))
-        SCCD_C.grid(row=9, column=2)
-        SCCD_Center = tk.Button(top, text="SCCD: Center", command=lambda: self._setTrueAndExit(top, CCDLabel="SCCD"))
-        SCCD_Center.grid(row=9, column=3)
-        
-        # ECCD
-        Separator(top, orient="horizontal").grid(row=10, column=0, columnspan=4, sticky='ew')
-        tk.Label(top, text="ECCD").grid(row=11, column=0, columnspan=2, sticky='W')
-        ECCD_A = tk.Button(top, text="ECCD: A", command=lambda: self._setTrueAndExit(top, CCDLabel="ECCD", trianglePointLabel="A"))
-        ECCD_A.grid(row=12, column=0)
-        ECCD_B = tk.Button(top, text="ECCD: B", command=lambda: self._setTrueAndExit(top, CCDLabel="ECCD", trianglePointLabel="B"))
-        ECCD_B.grid(row=12, column=1)
-        ECCD_C = tk.Button(top, text="ECCD: C", command=lambda: self._setTrueAndExit(top, CCDLabel="ECCD", trianglePointLabel="C"))
-        ECCD_C.grid(row=12, column=2)
-        ECCD_Center = tk.Button(top, text="ECCD: Center", command=lambda: self._setTrueAndExit(top, CCDLabel="ECCD"))
-        ECCD_Center.grid(row=12, column=3)
-        
-        # CCCD
-        Separator(top, orient="horizontal").grid(row=13, column=0, columnspan=4, sticky='ew')
-        tk.Label(top, text="CCCD").grid(row=14, column=0, columnspan=2, sticky='W')
-        CCCD_A = tk.Button(top, text="CCCD: A", command=lambda: self._setTrueAndExit(top, CCDLabel="CCCD", trianglePointLabel="A"))
-        CCCD_A.grid(row=15, column=0)
-        CCCD_B = tk.Button(top, text="CCCD: B", command=lambda: self._setTrueAndExit(top, CCDLabel="CCCD", trianglePointLabel="B"))
-        CCCD_B.grid(row=15, column=1)
-        CCCD_C = tk.Button(top, text="CCCD: C", command=lambda: self._setTrueAndExit(top, CCDLabel="CCCD", trianglePointLabel="C"))
-        CCCD_C.grid(row=15, column=2)
-        CCCD_Center = tk.Button(top, text="CCCD: Center", command=lambda: self._setTrueAndExit(top, CCDLabel="CCCD"))
-        CCCD_Center.grid(row=15, column=3)
-           
-    def _setTrueAndExit(self, windowVariable, CCDLabel, trianglePointLabel=" "):
-        self.CCDSelection = CCDLabel
-        if trianglePointLabel != " ":
-            self.trianglePointSelection = trianglePointLabel
-        windowVariable.destroy()
-        
