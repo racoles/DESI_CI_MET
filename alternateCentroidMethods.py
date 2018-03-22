1569bc9b1124eb718503031d1d236867528db2e4
Coles <coles.81@asc.ohio-state.edu>
2018-03-22 14:47:08 -0400

    adding Origin check

--- alternateCentroidMethods.py
+++ alternateCentroidMethods.py
@@ -9,12 +9,11 @@ alternateCentroidMethods
 '''
 
 # Import #######################################################################################
-import os, sys, getopt, subprocess, shlex, signal
+#import os, sys, getopt, subprocess, shlex, signal
 import math
 import numpy as np
-from astropy.io import fits
-
-import scipy
+#from astropy.io import fits
+#import scipy
 from scipy import ndimage
 from scipy.optimize import curve_fit
 ################################################################################################
