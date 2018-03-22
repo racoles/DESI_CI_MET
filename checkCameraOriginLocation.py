94aa846b43b3c908f137cf828e726f7169dc5cdb
Coles <coles.81@asc.ohio-state.edu>
2018-03-22 13:58:37 -0400

    adding Origin check

--- checkCameraOriginLocation.py
+++ checkCameraOriginLocation.py
@@ -98,7 +98,7 @@ class checkCameraOriginLocation(object):
                     ":\n CS5 (X = " + format(fC.trianglePonitCCDLocationsCS5[self.CCDSelection + self.trianglePointLabel][0] + ((rows-self.pixelDistanceToCheckPoint)*(pixelSize/1000)), '.3f') +
                      "mm, Y = " + format(fC.trianglePonitCCDLocationsCS5[self.CCDSelection + self.trianglePointLabel][1] + ((columns-self.pixelDistanceToCheckPoint)*(pixelSize/1000)), '.3f') + "mm)" + 
                     "\nYou should be able to see the origin of the sensor using the SBIG ST-i. A pinhole projected onto the SBIG STXL at this point show show up in a SBIG STXL at pixel location " +
-                    "( row = " + str(self.pixelDistanceToCheckPoint) + ", column = " + str(self.pixelDistanceToCheckPoint) + ")")
+                    "(row = " + str(self.pixelDistanceToCheckPoint) + ", column = " + str(self.pixelDistanceToCheckPoint) + ")")
             
         elif self.CCDSelection != '' and self.trianglePointLabel == '':
             #pinhole is at CCD center
@@ -109,7 +109,7 @@ class checkCameraOriginLocation(object):
                     "To check SBIG STXL sensor origin location, move to CCD pixel location (" + str(self.pixelDistanceToCheckPoint) + "," + str(self.pixelDistanceToCheckPoint) + ")" + 
                     ":\n CS5 (X = " + str(fC.CCDLocationsCS5[self.CCDSelection][0] + ((rows-self.pixelDistanceToCheckPoint)*(pixelSize/1000))) +
                      "mm, Y = " + str(fC.CCDLocationsCS5[self.CCDSelection][1] + ((columns-self.pixelDistanceToCheckPoint)*(pixelSize/1000))) + "mm)" + 
-                    "\nYou should be able to see the origin of the sensor using the SBIG ST-i. A pinhole projected onto the SBIG STXL at this point show show up in a SBIG STXL at pixel location " +
+                    "\nYou should be able to see the origin of the sensor using the SBIG ST-i. A pinhole projected onto the SBIG STXL at this point shouls show up in a SBIG STXL at pixel location " +
                     "(row = " + str(self.pixelDistanceToCheckPoint) + ", column = " + str(self.pixelDistanceToCheckPoint) + ")")
             
         else:
