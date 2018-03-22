bd68841255edf96209a2d8dd4c0570a332494ec1
Coles <coles.81@asc.ohio-state.edu>
2018-03-22 16:26:53 -0400

    adding Origin check

--- checkCameraOriginLocation.py
+++ checkCameraOriginLocation.py
@@ -87,7 +87,7 @@ class checkCameraOriginLocation(object):
             CS5OriginX = fC.trianglePonitCCDLocationsCS5[self.CCDSelection + self.trianglePointLabel][0] + (rows*(pixelSize/1000))
             CS5OriginY = fC.trianglePonitCCDLocationsCS5[self.CCDSelection + self.trianglePointLabel][1] + (columns*(pixelSize/1000))
             faah.pageLogging(consoleLog, logFile, 
-                    "Distance from pinhole center to sensor origin: " + format(hypotenuse, '.3f') + "pixels or " + format(hypotenuse*pixelSize, '.3f') + 'um\n' +
+                    "Distance from pinhole center to sensor origin:\n " + format(hypotenuse, '.3f') + "pixels = " + format(hypotenuse*pixelSize, '.3f') + 'um = ' + format(hypotenuse*pixelSize/1000, '.3f') + "mm\n"
                     "To check SBIG STXL sensor origin location, move to CCD pixel location (" + str(self.pixelDistanceToCheckPoint) + "," + str(self.pixelDistanceToCheckPoint) + ")" + 
                     ":\n CS5 (X = " + format(fC.trianglePonitCCDLocationsCS5[self.CCDSelection + self.trianglePointLabel][0] + ((rows-self.pixelDistanceToCheckPoint)*(pixelSize/1000)), '.3f') +
                      "mm, Y = " + format(fC.trianglePonitCCDLocationsCS5[self.CCDSelection + self.trianglePointLabel][1] + ((columns-self.pixelDistanceToCheckPoint)*(pixelSize/1000)), '.3f') + "mm)" + 
