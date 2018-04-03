35e70a96fce9436e2054cba93f161db14ddec523
Coles <coles.81@asc.ohio-state.edu>
2018-04-03 11:20:28 -0400

    adding Origin check

--- CCDOpsPlanetMode.py
+++ CCDOpsPlanetMode.py
@@ -1,14 +1,12 @@
 '''
 @title CCDOpsPlanetMode
 @author: Rebecca Coles
-Updated on Jan 23, 2018
+Updated on Apr 03, 2018
 Created on Jan 22, 2018
 
 CCDOpsPlanetMode
 This module holds a series of functions that I use find offsets when using the
 CCDOps software in "planet" imaging mode.
-
-Modules:
 '''
 
 # Import #######################################################################################
